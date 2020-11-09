c5510cddd431fe89e77fc7a6c24551851b4c0a99
timstetter <tstetter@gmail.com>
2020-11-09 10:22:32 -0500

    HTML added table, script created arrays and functions for price and total

--- script.js
+++ script.js
@@ -1,68 +1,21 @@
-//Create an array for items and price
+let nameArray = [];
+let priceArray = [];
 
-let item1 = {
-	name: 'Bread',
-	price: 1.99
-}
-let item2 = {
-	name: 'Milk',
-	price: 2.49
-}
-let item3 = {
-	name: 'Eggs',
-	price: 0.99
-}
-let item4 = {
-	name: 'Butter',
-	price: 2.79
+function addToCart(productName, productPrice) {
+	nameArray.push(productName);
+	priceArray.push(productPrice);
 }
 
-let items = [
-	item1, item2, item3, item4
-];
-
-let total = 0;
-let button = '<button id="add">Add to List</button>';
-
-//let addEvent = document.querySelectorAll('#add')[0];
-//addEvent.addEventListener('click', addToTotal);
-
-let listEvent = document.querySelectorAll('#listbutton')[0];
-listEvent.addEventListener('click', listItemsToHtml);
-
-let totalEvent = document.querySelectorAll('#displaytotal')[0];
-totalEvent.addEventListener('click', displayTotal);
-
-function displayTotal() {
-	let html = '';
-	html += 'Total: ' + total;
-
-	document.getElementById('displaytotal').innerHTML = html;
-}
-
-function addToTotal() {
-
-	total += items.price;
-}
-
-
-function listItemsToHtml() {
-	let html = '';
+function printCart() {
+	let i;
+	let total = 0;
 	
-
-
-	for (let i = 0; i < items.length; i++) {
-		html += 'Item: ' +
-			items[i].name +
-			' Price: ' + items[i].price + button + '<br />';
+	for (i = 0; i < priceArray.length; i++) {
+		total += priceArray[i];
 	}
-	document.getElementById('list_items').innerHTML = html;
-
-
+	
+	let totalCart = document.getElementById("totalcart");
+	
+	totalCart.innerHTML = "Total: $" + total;
+	
 }
-
-
-
-
-
-
